f9a65e2f7304b4efdbcf8e23ed54adccc0780579
LuizMCC <foxymaneiro2@gmail.com>
2024-10-01 22:05:32 -0300

    index.html foi configurado para ser exibido

--- src/main/java/com/veryona/jogos/controller/JogoController.java
+++ src/main/java/com/veryona/jogos/controller/JogoController.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.veryona.jogos.controller;
 
-/**
- *
- * @author Luiz
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class JogoController {
-    
+
+    @GetMapping("/saudacao")
+    public String mostraSaudacao() {
+        return "saudacao";
+    }
 }
