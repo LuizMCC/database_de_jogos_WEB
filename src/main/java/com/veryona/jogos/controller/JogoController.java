673d4ffd63470e874e92875ab6d4c98cc76a8784
LuizMCC <foxymaneiro2@gmail.com>
2024-10-15 20:25:02 -0300

    Colocado a tabela principal mais um navbar no menu

--- src/main/java/com/veryona/jogos/controller/JogoController.java
+++ src/main/java/com/veryona/jogos/controller/JogoController.java
@@ -1,13 +1,25 @@
 package com.veryona.jogos.controller;
 
+import com.veryona.jogos.model.Jogo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JogoController {
 
-    @GetMapping("/saudacao")
+    @GetMapping("/menu")
     public String mostraSaudacao() {
-        return "saudacao";
+        return "index";
+        }
+    @GetMapping("/jogo")
+    public String returnJogo(Model model, String ID, String nome){
+        Jogo j = new Jogo();
+        ID = "1234";
+        nome = "Jogo1";
+        j.setID(ID);
+        j.setNome(nome);
+        model.addAttribute("jogo", j);
+        return "index";
     }
 }
