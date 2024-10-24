87b757185154e64028047203efe3719f9f1f8c91
LuizMCC <foxymaneiro2@gmail.com>
2024-10-24 18:13:57 -0300

    classe 'console' adicionada

---
+++ src/main/java/com/veryona/jogos/model/Console.java
@@ -0,0 +1,14 @@
+package com.veryona.jogos.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Console {
+    String ID;
+    String empresa;
+    String nome;
+}
