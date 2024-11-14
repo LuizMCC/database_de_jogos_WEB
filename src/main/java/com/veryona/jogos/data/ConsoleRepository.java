1d4b392a3159cba5212194f7233ca477fbecbba1
LuizMCC <foxymaneiro2@gmail.com>
2024-11-14 15:56:30 -0300

    'Console' e 'ConsoleEntity' também foram unificadas

--- src/main/java/com/veryona/jogos/data/ConsoleRepository.java
+++ src/main/java/com/veryona/jogos/data/ConsoleRepository.java
@@ -1,9 +1,10 @@
 package com.veryona.jogos.data;
 
+import com.veryona.jogos.model.Console;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConsoleRepository extends JpaRepository<ConsoleEntity, String>{
+public interface ConsoleRepository extends JpaRepository<Console, String>{
     
 }
